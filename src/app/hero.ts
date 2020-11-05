30522893f1bb6d4cd5aa87f15b76af62b12481ff
sorinaioja <sorina_ioja@yahoo.com>
2020-11-05 14:58:05 +0000

    hero class

---
+++ src/app/hero.ts
@@ -0,0 +1,9 @@
+export class Hero {
+  constructor(
+    public name: string,
+    public alterEgo: string,
+    public description: string,
+    public photo: string,
+  ) {}
+  // constructorul are doar parametrii, momentan corpul este gol
+}
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'jsh-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
-  public name = 'Aso';
-  public cnt = 0;
+  public hero:Hero;
+
   constructor() { }
 
   ngOnInit() {
-  }
-  sayHello() {
-    alert('Hello ' + this.name);
-  }
-  cntShow(){
-    this.cnt++;
-    alert('Apasat de:' + this.cnt);
-  }
+    this.hero = new Hero(
+    'Batman',
+    'Bruce Wayne',
+    "One of the most iconic fictional characters in the world, Batman has dedicated his life to an endless crusade, a war on all criminals in the name of his murdered parents, who were taken from him when he was just a child. Since that tragic night, he has trained his body and mind to near physical perfection to be a self-made Super Hero. He's developed an arsenal of technology that would put most armies to shame. And he's assembled teams of his fellow DC Super Heroes, like the Justice League, the Outsiders and Batman, Incorporated.",
+    'assets/batman.jpg'
+  );
+   }
+   callHero() {
+    alert(this.hero.name+ " (" + this.hero.alterEgo + ") has been called!" );
+    }
 
 }
